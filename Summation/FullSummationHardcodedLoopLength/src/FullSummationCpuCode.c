f0d9d0d40102fd40bba4e3046bfb868bec6b871d
Pavel Burovskiy <p.burovskiy@imperial.ac.uk>
2013-10-18 17:45:32 +0100

    Adding project for full summation using stream offsets

---
+++ Summation/FullSummationHardcodedLoopLength/src/FullSummationCpuCode.c
@@ -0,0 +1,47 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Maxfiles.h"
+#include "MaxSLiCInterface.h"
+
+int main(void)
+{
+    const int inSize = 384;
+    const int loopLength = 16;
+    const int minimalPCIeStreamLength = 4;
+    float *in       = malloc(sizeof(float)*inSize);
+    float *full     = malloc(sizeof(float)*minimalPCIeStreamLength);
+    float *partial  = malloc(sizeof(float)*loopLength);
+
+    float sum = 0;
+    for(int i = 0; i < inSize; ++i) {
+        in[i]  = i+1;
+        sum   += i+1;
+    }
+
+    printf("Running on DFE.\n");
+    FullSummation(inSize, in, full, partial);
+
+    printf("output from DFE, full summation (stream): ");
+    float dfeTotalSum = full[minimalPCIeStreamLength-1];
+    float dfeSum = 0;
+    for(int i = 0; i < minimalPCIeStreamLength; ++i)
+    {
+        printf(" %f", full[i]);
+    }
+    printf("\noutput from DFE: partial sums: ");
+    for(int i = 0; i < loopLength; ++i)
+    {
+        dfeSum += partial[i];
+        printf(" %f", partial[i]);
+    }
+    printf("\nTotal sum from DFE             = %f", dfeTotalSum);
+    printf("\nSum of partial values from DFE = %f", dfeSum);
+    printf("\nCPU calculated sum             = %f\nDone.\n",sum);
+    free(in);
+    free(partial);
+    free(full);
+
+    return !((dfeSum == sum) && (dfeSum == dfeTotalSum));
+}
