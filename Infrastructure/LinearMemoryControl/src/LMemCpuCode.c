b4cc4e384bb7d803cf734eeccfc275eb2564e5a2
thomasccp <thomasccp@msn.com>
2013-10-22 17:47:35 +0100

    Removing package name. Adding support of quarter mode in MAIA.

--- Infrastructure/LinearMemoryControl/src/LMemCpuCode.c
+++ Infrastructure/LinearMemoryControl/src/LMemCpuCode.c
@@ -1,15 +1,5 @@
-/**
- * Document: Manager Compiler Tutorial (maxcompiler-manager-tutorial.pdf)
- * Chapter: 2      Example: 1      Name: LMem Example
- * MaxFile name: LMemExample
- * Summary:
- *     Creates a pair of arrays and writes each array to a separate location
- *     in LMem on the DFE. A Kernel running in the DFE reads the arrays from LMem
- *     computes their element-wise sum, and writes the result to a third LMem
- *     location. When this operation is complete, the CPU application reads the
- *     array of sums from the LMem, and verifies that they have been computed
- *     correctly. Groups all memory streams into a single control group.
- */
+/*** Linear memory control with support of quarter rate mode */
+
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
@@ -17,7 +7,7 @@
 #include <MaxSLiCInterface.h>
 #include "Maxfiles.h"
 
-void LMemExampleCPU(int size, int32_t *inA, int32_t *inB, int32_t *outData)
+void LMemCPU(int size, int32_t *inA, int32_t *inB, int32_t *outData)
 {
 	for (int i = 0; i < size; i++) {
 		outData[i] = inA[i] + inB[i];
@@ -48,18 +38,18 @@ int main()
 	}
 
 	printf("Loading DFE memory.\n");
-	LMemExample_writeLMem(size, 0, inA);
-	LMemExample_writeLMem(size, size, inB);
+	LMem_writeLMem(size, 0, inA);
+	LMem_writeLMem(size, size, inB);
 
 	printf("Running DFE.\n");
-	LMemExample(size);
+	LMem(size);
 
 	int32_t *expected = malloc(sizeBytes);
-	LMemExampleCPU(size, inA, inB, expected);
+	LMemCPU(size, inA, inB, expected);
 
 	printf("Reading DFE memory.\n");
 	int32_t *outData = malloc(sizeBytes);
-	LMemExample_readLMem(size, 2 * size, outData);
+	LMem_readLMem(size, 2 * size, outData);
 
 	int status = check(size, outData, expected);
 	if (status)
