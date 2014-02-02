2632378ccae321d9b9f8b64fc6d72ed4f84d68c4
Pavel Burovskiy <p.burovskiy@imperial.ac.uk>
2014-02-02 00:06:08 +0000

    Fix to logarithmic complexity accumulator. Works well for large enough inputs

--- Summation/FullSummationLogarithmicCost/src/FullSummationLogarithmicCostCpuCode.c
+++ Summation/FullSummationLogarithmicCost/src/FullSummationLogarithmicCostCpuCode.c
@@ -1,14 +1,4 @@
-/*
-
- This project implements full summation on DFE with fixed length of
- feedback summation loop. The length of this loop is hardcoded so that it
- exceeds the summator latency (important for floating point arithmetic
- adder which has latency 13, for FPGAs used in MAX4). Full summation
- is achieved by summation of all output values using stream offsets.
-
- This is probably an implementation with maximal hardware costs.
-
- Tested in simulation mode, has NOT been tested on hardware.
+/***
 
 */
 #include <math.h>
@@ -21,31 +11,31 @@
 int main(void)
 {
 
-    const int inSize = 12;
-    float *in        = malloc(sizeof(float)*inSize);
+    const int inSize = 32;
+    double *in        = malloc(sizeof(double)*inSize);
 
     // these two constants are defined in Maxfiles.h
-    float *full      = malloc(sizeof(float)*FullSummationLogarithmicCost_minimalPciStreamLength);
-    float *partial   = malloc(sizeof(float)*inSize);//FullSummationLogarithmicCost_schemeLatency);
+    double *full      = malloc(sizeof(double)*FullSummationLogarithmicCost_minimalPciStreamLength);
+    double *partial   = malloc(sizeof(double)*inSize);
 
-    float sum = 0;
+    double sum = 0;
     for(int i = 0; i < inSize; ++i) {
         in[i]  = i+1;
         sum   += i+1;
     }
 
     printf("Running on DFE.\n");
-    FullSummationLogarithmicCost(inSize, in, partial);//full, 
+    FullSummationLogarithmicCost(inSize, in, full,partial);
 
     printf("output from DFE, full summation (stream): ");
-    float dfeTotalSum = full[FullSummationLogarithmicCost_minimalPciStreamLength-1];
-    float dfeSum = 0;
+    double dfeTotalSum = full[0];
+    double dfeSum = 0;
     for(int i = 0; i < FullSummationLogarithmicCost_minimalPciStreamLength; ++i)
     {
         printf(" %f", full[i]);
     }
     printf("\noutput from DFE: partial sums: ");
-    for(int i = 0; i < inSize; ++i)//FullSummationLogarithmicCost_schemeLatency
+    for(int i = inSize-FullSummationLogarithmicCost_numPartialSums; i < inSize; ++i)
     {
         dfeSum += partial[i];
         printf(" %f", partial[i]);
