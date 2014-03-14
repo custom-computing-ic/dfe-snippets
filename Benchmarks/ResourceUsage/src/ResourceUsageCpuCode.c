1a711dd72db68abadfcb70a1cc7c5224efa4141e
Paul Grigoras <paul.grigoras90@gmail.com>
2014-03-14 18:03:26 +0000

    Remove unused arrays.

--- Benchmarks/ResourceUsage/src/ResourceUsageCpuCode.c
+++ Benchmarks/ResourceUsage/src/ResourceUsageCpuCode.c
@@ -17,7 +17,6 @@ int main(void)
 
   int *a = malloc(sizeof(int) * inSize);
   int *b = malloc(sizeof(int) * inSize);
-  int *expected = malloc(sizeof(int) * inSize);
 
   int *intRes = malloc(sizeof(int) * inSize);
   float *spRes = malloc(sizeof(float) * inSize);
@@ -26,7 +25,6 @@ int main(void)
   for(int i = 0; i < inSize; ++i) {
     a[i] = i + 1;
     b[i] = i - 1;
-    expected[i] = 2 * i;
   }
 
   printf("Running on DFE.\n");
@@ -35,14 +33,13 @@ int main(void)
   for (int i = 0; i < inSize; i++) {
     int exp = 2 * a[i] + a[i] * b[i];
     if (intRes[i] != exp ||
-	spRes[i] != exp ||
-	dpRes[i] != exp) {
+        spRes[i] != exp ||
+        dpRes[i] != exp) {
       printf("Output from DFE did not match CPU: %d : %d != %d\n",
-        i, intRes[i], exp);
+             i, intRes[i], exp);
       return 1;
     }
   }
-
 
   printf("Test passed!\n");
   return 0;
