c6fde5383b3ff3bd53396f011aca9e47c862e27e
Pavel Burovskiy <p.burovskiy@imperial.ac.uk>
2014-03-20 01:36:46 +0000

    Updating CPU side

--- Benchmarks/ResourceUsage/src/ResourceUsageCpuCode.c
+++ Benchmarks/ResourceUsage/src/ResourceUsageCpuCode.c
@@ -6,9 +6,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "Maxfiles.h"
 #include "MaxSLiCInterface.h"
+
+int intCpuResult(int a, int b)
+{
+    int sum  = a + b;
+    int sub  = a - b;
+    int prod = a * b;
+    int div  = a / b;
+
+    int imod1   = a % 3;          // = 2^2 - 1
+    int imod2   = b % 32767;      // = 2^15 - 1
+    int imod3   = b % 2147483647; // = 2^31 - 1 , 1345345345
+
+    int iexp    = exp(b);
+    int isqrt   = sqrt(b);
+    int isin    = sin(b);
+    int icos    = cos(b);
+    int imax    = (a >= b)? a : b; // max(a, b);
+    int imin    = (a >= b)? b : a; // min(a, b);
+    int iabs    = abs(iexp);
+
+    int intArithmetic  = sum + sub + prod + div;
+    int intKernelMaths = 
+                       iexp + isqrt + isin + icos + imax + imin + iabs +
+                       imod1 + imod2 + imod3;
+    return intArithmetic + intKernelMaths;
+}
 
 int main(void)
 {
@@ -29,16 +56,29 @@ int main(void)
   }
 
   printf("Running on DFE.\n");
-  ResourceUsage(inSize, a,a,a, b,b,b, dpRes, intRes, mpRes, spRes);
+  ResourceUsage(inSize, a, a, a, b, b, b, dpRes, intRes, mpRes, spRes);
+
+  float  spTol = 1e-8;
+  double dpTol = 1e-16;
 
   for (int i = 0; i < inSize; i++) {
-    int exp = 2 * a[i] + a[i] * b[i];
-    if (intRes[i] != exp ||
-        spRes[i] != exp ||
-        dpRes[i] != exp) {
-      printf("Output from DFE did not match CPU: %d : %d != %d\n",
-             i, intRes[i], exp);
-      return 1;
+    int intExp = intCpuResult(a[i],b[i]);
+    float exp = 2 * a[i] + a[i] * b[i];
+    if (intRes[i] != intExp){
+        printf("Integer output from DFE did not match CPU: %d : %d != %d\n", i, intRes[i], intExp);
+        return 1;
+    }
+    if ((spRes[i] - exp) > spTol){
+        printf("Single precision output from DFE did not match CPU: %d : %f != %f\n", i, spRes[i], exp);
+        return 1;
+    }
+    if ((mpRes[i] - exp) > spTol){
+        printf("Middle precision output from DFE did not match CPU: %d : %f != %f\n", i, mpRes[i], exp);
+        return 1;
+    }
+    if ((dpRes[i] - exp) > dpTol){
+        printf("Double precision output from DFE did not match CPU: %d : %f != %f\n", i, dpRes[i], exp);
+        return 1;
     }
   }
 
