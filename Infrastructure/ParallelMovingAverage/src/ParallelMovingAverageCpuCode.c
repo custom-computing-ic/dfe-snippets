0ec0a9451436915c28174277912178e6c03fccd8
Paul Grigoras <paul.grigoras90@gmail.com>
2014-05-20 01:49:18 +0100

    Support both maxelerOS groups and manual allocation of DFEs.

--- Infrastructure/ParallelMovingAverage/src/ParallelMovingAverageCpuCode.c
+++ Infrastructure/ParallelMovingAverage/src/ParallelMovingAverageCpuCode.c
@@ -11,7 +11,41 @@
 #include "MaxSLiCInterface.h"
 
 // Using MaxelerOS managed groups of DFEs
-void MovingAverageDFE_groups(int width, int n, int *in, int* out, int numEngines) {
+void MovingAverageDFE_groups(int numEngines,
+                             max_file_t* maxfile,
+                             ParallelMovingAverage_actions_t** actions)
+{
+  max_group_t *group = max_load_group(maxfile, MAXOS_EXCLUSIVE, "local", numEngines);
+
+#pragma omp parallel for
+  for (int i = 0; i < numEngines; i++)
+    ParallelMovingAverage_run_group(group, actions[i]);
+
+  max_unload_group(group);
+
+}
+
+// Using max_load/max_unload to load the bitstream on specific DFEs,
+// may be required for more advanced scheduling
+void MovingAverageDFE_custom(int numEngines,
+                             max_file_t* maxfile,
+                             ParallelMovingAverage_actions_t** actions,
+                             char** dfeIds)
+{
+#pragma omp parallel for
+  for (int i = 0; i < numEngines; i++) {
+    char id[100];
+    strncpy(id, "local:", 100);
+    strcat(id, dfeIds[i]);
+    max_engine_t *engine = max_load(maxfile, id);
+    ParallelMovingAverage_run(engine, actions[i]);
+    max_unload(engine);
+  }
+}
+
+
+void MovingAverageDFE(int width, int n, int *in, int* out,
+		      int numEngines, char** dfeIds, bool useGroups) {
   if (width != 3) {
     printf("Error, only moving average of width three supported!");
     exit(1);
@@ -37,11 +71,11 @@ void MovingAverageDFE_groups(int width, int n, int *in, int* out, int numEngines
   }
 
   max_file_t *maxfile = ParallelMovingAverage_init();
-  max_group_t *group = max_load_group(maxfile, MAXOS_EXCLUSIVE, "1", numEngines);
 
-#pragma omp parallel for
-  for (int i = 0; i < numEngines; i++)
-    ParallelMovingAverage_run_group(group, actions[i]);
+  if (useGroups)
+    MovingAverageDFE_groups(numEngines, maxfile, actions);
+  else
+    MovingAverageDFE_custom(numEngines, maxfile, actions, dfeIds);
 
   // need to redo the values around the boundaries between the batches
   for (int i = 0; i < numEngines; i++) {
@@ -49,19 +83,25 @@ void MovingAverageDFE_groups(int width, int n, int *in, int* out, int numEngines
     for (int j = 0; j < width / 2; j++) {
       int pos1 = boundary + j;
       if (i != 0)
-	out[pos1] = (in[pos1 - 1] + in[pos1] + in[pos1 + 1]) / 3;
+        out[pos1] = (in[pos1 - 1] + in[pos1] + in[pos1 + 1]) / 3;
 
       int pos2 = boundary + itemsPerDfe - j - 1;
       if (i != numEngines - 1)
-	out[pos2] = (in[pos2 - 1] + in[pos2] + in[pos2 + 1]) / 3;
+        out[pos2] = (in[pos2 - 1] + in[pos2] + in[pos2 + 1]) / 3;
     }
 
   }
 
-  max_unload_group(group);
   max_file_free(maxfile);
+}
 
-  printf("Test passed!\n");
+void check_results(int n, int* out, int* exp) {
+  for (int i = 1; i < n - 1; i++)
+    if (out[i] != exp[i]) {
+      printf("Output from DFE did not match CPU: %d : %d != %d\n",
+             i, out[i], exp[i]);
+      exit(1);
+    }
 }
 
 int main(int argc, char** argv) {
@@ -72,23 +112,23 @@ int main(int argc, char** argv) {
   int *out = calloc(n, sizeof(int));
   for(int i = 0; i < n; ++i)
     a[i] = i + 1;
-
-  printf("Running on DFE.\n");
-  MovingAverageDFE_groups(3, n, a, out, numEngines);
-
-  // check results
   int *exp = calloc(n, sizeof(int));
   for (int i = 1; i < n - 1; i++)
     exp[i] = (a[i - 1] + a[i] + a[i + 1]) / 3;
-  for (int i = 1; i < n - 1; i++)
-    if (out[i] != exp[i]) {
-      printf("Output from DFE did not match CPU: %d : %d != %d\n",
-             i, out[i], exp[i]);
-      return 1;
-    }
+
+  char *dfeIds[] = {"1", "2"};
+  printf("Running on DFE with groups.\n");
+  MovingAverageDFE(3, n, a, out, numEngines, dfeIds, true);
+  check_results(n, a, exp);
+
+  printf("Running on DFE with custom mode.\n");
+  MovingAverageDFE(3, n, a, out, numEngines, dfeIds, false);
+  check_results(n, a, exp);
 
   free(a);
   free(exp);
   free(out);
+
+  printf("Test passed!\n");
   return 0;
 }
