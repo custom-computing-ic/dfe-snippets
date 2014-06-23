44b28de26cc637eda6c5eb0fd3f3a34de69f479d
Pavel Burovskiy <p.burovskiy@imperial.ac.uk>
2014-06-23 20:10:04 +0100

    Fix the type cast problem

--- DataStructures/FetchSubTuple/src/FetchSubTupleCpuCode.cpp
+++ DataStructures/FetchSubTuple/src/FetchSubTupleCpuCode.cpp
@@ -14,21 +14,21 @@ int main(void)
   const int cycles = 40;
   const int inSize = cycles*FetchSubTuple_tupleSize;
 
-  std::vector<int> a(inSize), out(inSize, 0);
+  std::vector<double> in(inSize), out(inSize, 0);
 
   int sizes[] = {1,2,1,2, 3,1,2,1, 3,1,2,7, 7,7,8,7, 8,5,1,3, 6,5,8,7, 7,7,8,7, 8,7,7,7, 8,7,8,7, 7,7,8,7, 8,7,7,7, 8,7,8,7};
 
   int numElements = 0;
   for(int i = 0; i < inSize; ++i)
   {
-    a[i] = i + 1;
+    in[i] = i + 1;
 
     // the kernel skips 3 cycles => 40-3 = 37
     if (i < 37) numElements += sizes[i];
   }
 
   std::cout << "Running on DFE." << std::endl;
-  FetchSubTuple(cycles, inSize, &a[0], sizes, &out[0]);
+  FetchSubTuple(cycles, inSize, &in[0], sizes, &out[0]);
 
   std::cout << std::endl;
 
@@ -38,7 +38,7 @@ int main(void)
     printf("\ntuple %d with size %d : ", i, sizes[i]);
     for (int j = 0; j < FetchSubTuple_tupleSize; j++)
     {
-        printf("%d ", out[count+j]);
+        printf("%2.f ", out[count+j]);
     }
     count += FetchSubTuple_tupleSize;
   }
