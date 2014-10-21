46270be2e670679e50a275a72eb56621bff72429
Paul Grigoras <paul.grigoras90@gmail.com>
2014-10-21 17:40:55 +0100

    Change some UoM.

--- Infrastructure/InfinibandBenchmark/src/InfinibandBenchmarkCpuCode.cpp
+++ Infrastructure/InfinibandBenchmark/src/InfinibandBenchmarkCpuCode.cpp
@@ -13,22 +13,23 @@
 
 using namespace std;
 
-const double GB = pow(1024, 3) * 8;
-const double MB = pow(1024, 2) * 8;
+// All measurements should be in bytes
+const double GB = pow(1024, 3);
+const double MB = pow(1024, 2);
 
 
 /** Maxmimum possible bandwidth at given clock speed*/
 void printMaxInfinbandBandwidth() {
-    double clock = 200.0;
-    double inBitsPerCycle = 32 * 2;
-    double outBitsPerCycle = 32;
-    double max = (inBitsPerCycle + outBitsPerCycle) * clock * pow10(6) / GB;
-    cout << "Max possible bandwidth " << max << " GB/s" << endl;
+    double clock = 150.0;
+    double inBytesPerCycle = sizeof(int) * 2;
+    double outBytesPerCycle = sizeof(int);
+    double max = (inBytesPerCycle + outBytesPerCycle) * clock * 1E6 / GB;
+    cout << "Max possible bandwidth (@150 MHz Stream Clock): " << max << " GB/s" << endl;
 }
 
 
-double measuredInifibandBandwidth(long sizeInBits, double runtimeS) {
-    return 3 * size / MB / runtimeS;
+double measuredInifibandBandwidth(long sizeInBytes, double runtimeS) {
+    return sizeInBytes / MB / runtimeS;
 }
 
 
@@ -36,6 +37,7 @@ int main(void)
 {
 
     const long inSize = 2 * 384 * 1E6;
+    const long dataSizeBytes = 3 * inSize * sizeof(int);
 
     std::vector<int> a(inSize), b(inSize), expected(inSize), out(inSize, 0);
 
@@ -46,17 +48,19 @@ int main(void)
     }
 
     printMaxInfinbandBandwidth();
-    cout << "Running on DFE." << endl;
+    cout << "Running on DFE. Data size: " << dataSizeBytes / GB << " GB" << endl;
+
     struct timeval tv1, tv2;
     gettimeofday(&tv1, NULL);
     InfinibandBenchmark(inSize, &a[0], &b[0], &out[0]);
     gettimeofday(&tv2, NULL);
 
-    cout << "Data size " << inSize * sizeof(int) / GB  << " GBs" << endl;
     double runtimeS = ((tv2.tv_sec-tv1.tv_sec) * (double)1E6 +
                        (tv2.tv_usec-tv1.tv_usec)) / (double)1E6;
+
     cout << "Runtime (s) " << runtimeS << endl;
-    cout << "Bandwidth " << measuredInifibandBandwidth(inSize * sizeof(int),  runtimeS) << " MB/s " << endl;
+    cout << "Bandwidth " << measuredInifibandBandwidth(dataSizeBytes,  runtimeS);
+    cout << " MB/s " << endl;
 
     for (int i = 0; i < inSize; i++)
         if (out[i] != expected[i]) {
