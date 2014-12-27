e166e4b711cfefde6c44db3d70d448ea20dd75d8
Paul Grigoras <paul.grigoras90@gmail.com>
2014-12-27 10:10:35 +0000

    Queue more read commands leading to kernel stall. Add explanation for termination condition.

--- Test/TerminationCondition/Readme.md
+++ Test/TerminationCondition/Readme.md
@@ -1,10 +1,13 @@
 # About
 
 Simple passthrough design to test termination condition for a blocking
-SLiC call in the presence of memory streams. According to the manual
-the blocking call should wait until all memory streams have set the
-interrupt flag.
+SLiC call in the presence of memory streams.
 
-We run the kernel for N cycles and expect N output values. We queue
-2 * N data from on board DRAM. According to the manual this should
-stall, but it seems to work in simulation.
+According to the manual the blocking call should wait until all memory
+streams have set the interrupt flag. When a large number of bursts are
+left unread this can stall the kernel. This is probably because the
+data queue from memory fills up and the last memory read command
+(which shouls set the interrupt flat) is left unprocessed in the command queue.
+
+This behaviour can be reproduced with this snippet at between 64 - 128 bursts
+queued and unprocessed.
--- Test/TerminationCondition/src/TerminationConditionCpuCode.cpp
+++ Test/TerminationCondition/src/TerminationConditionCpuCode.cpp
@@ -13,14 +13,21 @@
 
 using namespace std;
 
-int main(void)
-{
+void run(int mult);
+
+int main(void) {
+	for (int i = 2; i < 256; i *= 2)
+		run(i);
+	return 0;
+}
+
+void run(int mult) {
 
   const int inSize = 384;
 
-  vector<int> a(2 * inSize),  expected(inSize), out(inSize, 0);
+  vector<int> a(mult * inSize),  expected(inSize), out(inSize, 0);
 
-  for(int i = 0; i < 2 * inSize; ++i) {
+  for(int i = 0; i < mult * inSize; ++i) {
     a[i] = i;
   }
 
@@ -29,19 +36,18 @@ int main(void)
   }
 
   cout << "Writing to DFE Memory...";
-  TerminationCondition_writeDRAM(2 * inSize * sizeof(int), 0, (uint8_t*)&a[0]);
+  TerminationCondition_writeDRAM(mult * inSize * sizeof(int), 0, (uint8_t*)&a[0]);
   cout << "done" << endl;
 
-  cout << "Running on DFE." << endl;
-  TerminationCondition(inSize, &out[0], 0, 2 * inSize * sizeof(int));
+  cout << "Running on DFE. Queued " << mult << " bursts." << endl;
+  TerminationCondition(inSize, &out[0], 0, mult * inSize * sizeof(int));
 
   for (int i = 0; i < inSize; i++)
     if (out[i] != expected[i]) {
       printf("Output from DFE did not match CPU: %d : %d != %d\n",
         i, out[i], expected[i]);
-      return 1;
+      exit(1);
     }
 
   cout << "Test passed!" << endl;
-  return 0;
 }
