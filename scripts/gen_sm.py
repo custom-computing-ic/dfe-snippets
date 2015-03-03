51454e50a1475a18980956f69ae55b23a0a093a0
Paul Grigoras <paul.grigoras90@gmail.com>
2015-03-03 10:38:09 +0000

    Add script to generate some code for output control state machine.

---
+++ scripts/gen_sm.py
@@ -0,0 +1,26 @@
+noutputs=16
+
+
+def main():
+
+    i = 0
+    print 'IF (inReady(', i, ')) {'
+    print '   in[', i, ' ].read <== true;'
+    print '}'
+    for i in range(1, noutputs):
+        print 'ELSE IF (inReady(', i, ')) {'
+        print '   in[', i, '].read <== true;'
+        print '}'
+
+    i = 0
+    print 'IF (inReady(', i, ')) {'
+    print '  consume(', i, ');'
+    print '}'
+    for i in range(1, noutputs):
+        print 'ELSE IF (inReady(', i, ')) {'
+        print '   consume(', i, ');'
+        print '}'
+
+
+if __name__ == '__main__':
+    main()
