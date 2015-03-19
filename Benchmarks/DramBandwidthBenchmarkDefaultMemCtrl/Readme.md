c7115724f6a5e7e94bfeef84c929511b56743596
Pavel Burovskiy <p.burovskiy@imperial.ac.uk>
2015-03-19 09:34:01 +0000

    Updating simple DRAM benchmark to use 2 DRAM streams: 58.11GB/s on Maia

--- Benchmarks/DramBandwidthBenchmarkDefaultMemCtrl/Readme.md
+++ Benchmarks/DramBandwidthBenchmarkDefaultMemCtrl/Readme.md
@@ -2,11 +2,10 @@
 
 This is a DRAM bandwidth benchmark with default memory controller and linear access pattern.
 
-We benchmark simple reads of a number of single precision vector entries. Vector
-is large enough (>1GB) to neglect any setup/initialisation costs. Calls to
-compute kernel are repeated several times to avoid run time fluctuations. We benchmark
-both FPGA and CPU memory bandwidth. On CPU our vector size yields out-of-cache memory
-throughput benchmark.
+We benchmark simple reads of a number of 64 bit integers per cycle split into 2
+DRAM streams. Vector is large enough (>1GB) to neglect any setup/initialisation
+costs. Calls to compute kernel are repeated several times to avoid run time
+fluctuations.
 
 # Parameters
 
@@ -16,16 +15,25 @@ throughput benchmark.
 
     - Kernel and memory frequency
 
-# Example:
+# Experience:
 
 For the kernel and memory frequencies 180Mhz and 733Mhz accordingly (Maia), reading
-48 and 96 floats (32 bit) per cycle gives the following figures:
+48 and 96 floats (32 bit) per cycle in a single DRAM stream gives the following figures:
 
 48 float32 values = 32080.29 MiB/s
 96 float32 values = 52270.56 MiB/s
 
 Test data set size is 1464.843750 MiB, vector with 384*10^6 float32 entries. Tests are
 repeated 50 times, time measured with microsecond(*) resolution with gettimeofday().
+
+
+
+For the kernel frequency 190Mhz and DRAM freq 733Mhz reading two 1536 bits vectors 
+per cycle gives:
+
+2 streams (24 entries 64 bit each; in total 384 bytes): 58.11 GB/s
+
+on 24GB dataset.
 
 (*) not always accurate
 
--- Benchmarks/DramBandwidthBenchmarkDefaultMemCtrl/src/DramBandwidthBenchmarkDefaultMemCtrlCpuCode.c
+++ Benchmarks/DramBandwidthBenchmarkDefaultMemCtrl/src/DramBandwidthBenchmarkDefaultMemCtrlCpuCode.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 
 #include "Maxfiles.h"
@@ -19,67 +20,50 @@ void iffree(void * p)
 int main(void)
 {
     const int repeatTimes = 50;
-    const int vectorSize = 384*1000000;
 
-    const double datasetSizeMiB = vectorSize*sizeof(float)/(1024*1024.0);
+    // size in bytes: 24GB, split into 2 streams
+    const uint64_t vectorSize = 24*(uint64_t)1024*1024*1024;
+    const double datasetGB = vectorSize/(1024.0*1024.0*1024.0);
+    // how many ticks the kernel should run: kernel reads 384 bytes per cycle
+    const uint64_t ticksToRun = vectorSize/384;
 
-    printf("\nvector size = %d\n",vectorSize);
-    printf("data set size %f MiB\n", datasetSizeMiB );
+    printf("\nvector size = %ld\n",vectorSize);
+    printf("data set size %f GB\n", datasetGB );
 
-    float *vector = malloc(sizeof(float)*vectorSize);
-    float *outstream_sum = malloc(sizeof(float)*DramBandwidthBenchmarkDefaultMemCtrl_minimalPciStreamLength);
-
-    for(int i = 0; i < vectorSize; ++i)
-    {
-        float x = (i + 1) % 4;
-        vector[i] = x*x;
-    }
-
-    printf("timing CPU implementation:\n");
+    uint64_t *outstream_sum = malloc(sizeof(uint64_t)*DramBandwidthBenchmarkDefaultMemCtrl_minimalPciStreamLength);
 
     struct timeval t1, t2;
+
+    printf("warming up+setting up bistream...\n");
+    fflush(stdout);
+    DramBandwidthBenchmarkDefaultMemCtrl(384, outstream_sum);
+
+    printf("\ntiming very short DFE run (what's the latency of running an action):");
     gettimeofday(&t1, NULL);
-        float sum = 0;
-        for(int j = 0; j < repeatTimes; ++j)
-        {
-            for(int i = 0; i < vectorSize; ++i)
-            {
-                const float v = vector[i] + 1;
-                sum += v;
-            }
-        }
+        DramBandwidthBenchmarkDefaultMemCtrl(384, outstream_sum);
     gettimeofday(&t2, NULL);
 
-    printf("ignore this number: %f\n", sum);
+    double short_microSeconds = (t2.tv_sec - t1.tv_sec) * 1000 * 1000 + (t2.tv_usec - t1.tv_usec);
+    double short_seconds = short_microSeconds/(1000.0*1000.0);
+    printf("\ndone on DFE in %.2f seconds (%.2f microsec)\n", short_seconds, short_microSeconds);
+    fflush(stdout);
 
-    double cpu_microSeconds = (t2.tv_sec - t1.tv_sec) * 1000 * 1000 + (t2.tv_usec - t1.tv_usec);
-    double cpu_seconds = cpu_microSeconds/(1000.0*1000.0);
-    double cpu_bandwidth = (datasetSizeMiB*repeatTimes)/cpu_seconds;
-
-    printf("\nDone on CPU in %.2f seconds (%.2f microsec), bandwidth %.2f MiB/s \n", cpu_seconds, cpu_microSeconds, cpu_bandwidth);
-    printf("\n");
-
-    // write vector data to DRAM
-    DramBandwidthBenchmarkDefaultMemCtrl_WriteLMem(vectorSize, vector);
-
-    printf("timing DFE implementation:");
-
+    printf("\nbenchmarking: ");
     gettimeofday(&t1, NULL);
-        for(int j = 0; j < repeatTimes; ++j)
-        {
-            DramBandwidthBenchmarkDefaultMemCtrl(vectorSize, outstream_sum);
-        }
+    for (int i = 0; i < repeatTimes; i++)
+    {
+        // vectorSize is size in bytes, internally this is split into 2 streams of 64bit (8byte) ints => 2*8 = 16
+        DramBandwidthBenchmarkDefaultMemCtrl(ticksToRun, outstream_sum);
+    }
     gettimeofday(&t2, NULL);
 
-    double dfe_microSeconds = (t2.tv_sec - t1.tv_sec) * 1000 * 1000 + (t2.tv_usec - t1.tv_usec);
+    double dfe_microSeconds = ((t2.tv_sec - t1.tv_sec) * 1000 * 1000 + (t2.tv_usec - t1.tv_usec))/repeatTimes;
     double dfe_seconds = dfe_microSeconds/(1000.0*1000.0);
-    double dfe_bandwidth = (datasetSizeMiB*repeatTimes)/dfe_seconds;
-
-    printf("\nDone on DFE in %.2f seconds (%.2f microsec), bandwidth %.2f MiB/s \n", dfe_seconds, dfe_microSeconds, dfe_bandwidth);
-
+    double dfe_bandwidth = datasetGB/dfe_seconds;
+    printf("\ndone on DFE in %.2f seconds (%.2f microsec) per try in avg, in %d tries, bandwidth %.2f GB/s \n", dfe_seconds, dfe_microSeconds, repeatTimes, dfe_bandwidth);
+    fflush(stdout);
 
     iffree(outstream_sum);
-    iffree(vector);
 
     return 0;
 }
