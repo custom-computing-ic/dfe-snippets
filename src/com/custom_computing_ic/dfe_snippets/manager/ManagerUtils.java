8f23a5777413b00410f1b58b901c007b359232e7
Paul Grigoras <paul.grigoras90@gmail.com>
2015-06-03 14:45:14 +0100

    Add dram write interface.

--- src/com/custom_computing_ic/dfe_snippets/manager/ManagerUtils.java
+++ src/com/custom_computing_ic/dfe_snippets/manager/ManagerUtils.java
@@ -6,6 +6,10 @@ import com.maxeler.maxcompiler.v2.managers.custom.stdlib.MemoryControllerConfig;
 import com.maxeler.maxcompiler.v2.managers.custom.stdlib.DebugLevel;
 import com.maxeler.maxcompiler.v2.managers.custom.CustomManager;
 import com.maxeler.maxcompiler.v2.managers.custom.stdlib.MemoryControllerConfig;
+import com.maxeler.maxcompiler.v2.managers.engine_interfaces.EngineInterface;
+import com.maxeler.maxcompiler.v2.managers.engine_interfaces.EngineInterface.*;
+import com.maxeler.maxcompiler.v1.managers.engine_interfaces.InterfaceParam;
+import com.maxeler.maxcompiler.v2.managers.engine_interfaces.CPUTypes;
 
 public class ManagerUtils {
 
@@ -18,6 +22,19 @@ public class ManagerUtils {
     manager.config.setMemoryControllerConfig(memCfg);
   }
 
+  // A generic DRAM write interface
+  public static EngineInterface interfaceWrite(String name, String fromCpuStream, String cpu2lmemStream) {
+    EngineInterface ei = new EngineInterface(name);
+    CPUTypes TYPE = CPUTypes.INT;
+    InterfaceParam size = ei.addParam("size_bytes", TYPE);
+    InterfaceParam start = ei.addParam("start_bytes", TYPE);
+    ei.setStream(fromCpuStream, CPUTypes.UINT8, size);
+    ei.setLMemLinear(cpu2lmemStream, start, size);
+    ei.ignoreAll(Direction.IN_OUT);
+    return ei;
+  }
+
+  // Enable debugging with Stream Status for the given manager
   public static void debug(CustomManager manager) {
     DebugLevel dbgLevel = new DebugLevel();
     dbgLevel.setHasStreamStatus(true);
