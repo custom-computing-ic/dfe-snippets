bfa73c06783e268493ff512b3592fd41d78847dc
Paul Grigoras <paul.grigoras90@gmail.com>
2015-06-04 13:19:05 +0100

    Measure timings.

--- test/LinearAlgebra/DenseMatrixVectorMultiply/src/DenseMatrixVectorMultiplyCpuCode.cpp
+++ test/LinearAlgebra/DenseMatrixVectorMultiply/src/DenseMatrixVectorMultiplyCpuCode.cpp
@@ -28,7 +28,6 @@ vec run_parallel(const Matrix& m, const vec& b, const int numThreads) {
   int n = m.size();
   omp_set_num_threads(numThreads);
   auto start_time = high_resolution_clock::now();
-#pragma omp parallel for
   for (int i = 0; i < 10; i++) {
     auto v = m * b;
   }
@@ -42,7 +41,7 @@ vec run_parallel(const Matrix& m, const vec& b, const int numThreads) {
 
 int main(void) {
 
-  long n = 3 * (1 << 4);
+  long n = 3 * (1 << 12);
   Matrix m(n);
   m.init_random();
   m.print_info();
@@ -50,26 +49,29 @@ int main(void) {
 
   vector<double> b(n, 0);
   vector<double> v(n, 1);
-  auto exp = run_parallel(m, v, 1);
-  //run_parallel(m, 2);
-  //run_parallel(m, 6);
-  //run_parallel(m, 12);
-  //run_parallel(m, 24);
+  auto exp = run_parallel(m, v, 24);
 
   //std::cout << "Running on DFE." << std::endl;
   long bsizeBytes = sizeof(double) * n;
 
+  auto start = high_resolution_clock::now();
   m.convert_to_strided_access(48);
+  print_clock_diff("Convert to strided took", start);
+
+  start = high_resolution_clock::now();
   DenseMatrixVectorMultiply_write(
       bsizeBytes * n,
       0,
       (uint8_t *)m.linear_access_pointer());
+  print_clock_diff("Write to DRAM took", start);
 
   cout << "Starting DFE run " << endl;
+  start = high_resolution_clock::now();
   DenseMatrixVectorMultiply(
       n,
       &v[0],
       &b[0]);
+  print_clock_diff("FPGA run took", start);
 
   for (int i = 0; i < n; ++i) {
     if (b[i] != exp[i]) {
