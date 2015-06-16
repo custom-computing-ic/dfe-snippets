12f0d72c1f361c817a83e684a6155cfeb35e4b60
Paul Grigoras <paul.grigoras90@gmail.com>
2015-06-16 14:41:51 +0100

    Header only sparse/utils.

--- include/dfesnippets/sparse/utils.hpp
+++ include/dfesnippets/sparse/utils.hpp
@@ -2,9 +2,10 @@
 #define UTILS_HPP
 
 #include <chrono>
-#include <string>
 #include <vector>
+#include <cmath>
 #include <iostream>
+#include <string>
 
 bool almost_equal(double a, double b);
 
@@ -89,5 +90,46 @@ std::vector<std::vector<T> > zip_vectors(std::vector<std::vector<T> > in) {
 void print_spmv_gflops(std::string item,
                        int nnzs,
                        std::chrono::high_resolution_clock::time_point end,
-                       std::chrono::high_resolution_clock::time_point start); 
+                       std::chrono::high_resolution_clock::time_point start);
+
+bool almost_equal(double a, double b) {
+return std::abs(a - b) <= 1E-10 * std::abs(a);
+}
+
+/** Returns the smallest number greater than bytes that is multiple of k. */
+int align(int bytes, int k) {
+return  k * (bytes / k + (bytes % k == 0 ? 0 : 1));
+}
+
+/** Print the difference between now and start */
+void print_clock_diff(std::string item,
+              std::chrono::high_resolution_clock::time_point start) {
+  using namespace std::chrono;
+auto end = high_resolution_clock::now();
+std::cout << item << " took ";
+std::cout << duration_cast<duration<double> >(end - start).count();
+std::cout << " seconds." << std::endl;
+}
+
+/** Print the difference between end and start */
+void print_clock_diff(std::string item,
+              std::chrono::high_resolution_clock::time_point end,
+              std::chrono::high_resolution_clock::time_point start) {
+  using namespace std::chrono;
+std::cout << item << " took ";
+std::cout << duration_cast<duration<double> >(end - start).count();
+std::cout << " seconds." << std::endl;
+}
+
+void print_spmv_gflops(std::string item,
+                       int nnzs,
+                       std::chrono::high_resolution_clock::time_point end,
+                       std::chrono::high_resolution_clock::time_point start) {
+  using namespace std::chrono;
+  std::cout << item << " est. GLOPS ";
+  std::cout << 2.0 * nnzs / duration_cast<duration<double> >(end - start).count() / 1E9;
+  std::cout << std::endl;
+}
+
+
 #endif
