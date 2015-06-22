5d479b4108a7cc8ad462afff8630bd5a8c56bf52
Paul Grigoras <paul.grigoras90@gmail.com>
2015-06-22 14:36:49 +0100

    Cleanups.

--- include/dfesnippets/sparse/utils.hpp
+++ include/dfesnippets/sparse/utils.hpp
@@ -7,20 +7,6 @@
 #include <iostream>
 #include <string>
 
-bool almost_equal(double a, double b);
-
-/** Returns the smallest number greater than bytes that is multiple of k. */
-int align(int bytes, int k);
-
-/** Print the difference between now and start */
-void print_clock_diff(std::string item,
-                      std::chrono::high_resolution_clock::time_point start);
-
-/** Print the difference between end and start */
-void print_clock_diff(std::string item,
-                      std::chrono::high_resolution_clock::time_point end,
-                      std::chrono::high_resolution_clock::time_point start);
-
 // Zip the vectors from in and merge the results;
 // if the vectors are unequal, pad with 0
 template <typename T>
@@ -87,44 +73,39 @@ std::vector<std::vector<T> > zip_vectors(std::vector<std::vector<T> > in) {
   return zipped;
 }
 
-void print_spmv_gflops(std::string item,
-                       int nnzs,
-                       std::chrono::high_resolution_clock::time_point end,
-                       std::chrono::high_resolution_clock::time_point start);
-
 bool almost_equal(double a, double b) {
-return std::abs(a - b) <= 1E-10 * std::abs(a);
+  return std::abs(a - b) <= 1E-10 * std::abs(a);
 }
 
 /** Returns the smallest number greater than bytes that is multiple of k. */
 int align(int bytes, int k) {
-return  k * (bytes / k + (bytes % k == 0 ? 0 : 1));
+  return  k * (bytes / k + (bytes % k == 0 ? 0 : 1));
 }
 
 /** Print the difference between now and start */
 void print_clock_diff(std::string item,
-              std::chrono::high_resolution_clock::time_point start) {
+    std::chrono::high_resolution_clock::time_point start) {
   using namespace std::chrono;
-auto end = high_resolution_clock::now();
-std::cout << item << " took ";
-std::cout << duration_cast<duration<double> >(end - start).count();
-std::cout << " seconds." << std::endl;
+  auto end = high_resolution_clock::now();
+  std::cout << item << " took ";
+  std::cout << duration_cast<duration<double> >(end - start).count();
+  std::cout << " seconds." << std::endl;
 }
 
 /** Print the difference between end and start */
 void print_clock_diff(std::string item,
-              std::chrono::high_resolution_clock::time_point end,
-              std::chrono::high_resolution_clock::time_point start) {
+    std::chrono::high_resolution_clock::time_point end,
+    std::chrono::high_resolution_clock::time_point start) {
   using namespace std::chrono;
-std::cout << item << " took ";
-std::cout << duration_cast<duration<double> >(end - start).count();
-std::cout << " seconds." << std::endl;
+  std::cout << item << " took ";
+  std::cout << duration_cast<duration<double> >(end - start).count();
+  std::cout << " seconds." << std::endl;
 }
 
 void print_spmv_gflops(std::string item,
-                       int nnzs,
-                       std::chrono::high_resolution_clock::time_point end,
-                       std::chrono::high_resolution_clock::time_point start) {
+    int nnzs,
+    std::chrono::high_resolution_clock::time_point end,
+    std::chrono::high_resolution_clock::time_point start) {
   using namespace std::chrono;
   std::cout << item << " est. GLOPS ";
   std::cout << 2.0 * nnzs / duration_cast<duration<double> >(end - start).count() / 1E9;
