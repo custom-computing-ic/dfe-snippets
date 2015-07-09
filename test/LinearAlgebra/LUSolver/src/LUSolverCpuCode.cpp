1ee7a73843bc00b6de7200b160a71f3d59ff6be7
Paul Grigoras <paul.grigoras90@gmail.com>
2015-07-09 11:38:15 +0100

    Generate tests for GEPP.

--- test/LinearAlgebra/LUSolver/src/LUSolverCpuCode.cpp
+++ test/LinearAlgebra/LUSolver/src/LUSolverCpuCode.cpp
@@ -23,8 +23,12 @@ class System {
     Matrix a;
     vector<double> x;
     vector<double> b;
+
     System(int _n) : n(_n), a(_n), x(_n, 0), b(_n, 0) {
       a.init(0);
+    }
+
+    void init() {
       // start with identity matrix
       // fill the first row with multiples of main diagonal elements
       for (int i = 0; i < n; i++) {
@@ -61,6 +65,29 @@ class System {
         }
       }
     }
+};
+
+// A system which must be solved with (at least) partial pivoting
+class GEPPSystem : public System {
+
+  public:
+  GEPPSystem(long _n) : System(_n) {}
+
+  virtual void fillMatrix() override {
+    // for the system to require pivoting, at least one row must have zero
+    // entries on the main diagonal: we don't fill the last row and just interchange
+    // it with another row at the end
+    for (int i = 1; i < n - 1; i++) {
+      for (int k = 0; k < n; k++) {
+        a(i, k) += i * ((k % 4) + 1) * a(0, k);
+      }
+    }
+
+    if (n - 1 == n / 2) {
+      cerr << "Error: can't build a nice GEPP System with n of size " << n << endl;
+    }
+    a.row_interchange(n - 1, n / 2);
+  }
 };
 
 // Solve the upper triangular system Ax = b using back substitution
@@ -129,36 +156,77 @@ vector<double> lusolve(const Matrix &a, vector<double> b) {
   return backSubstitute(a, b);
 }
 
-int main(void) {
 
+void test_simple_ge(bool verbose = false) {
   long n = 100;
 
   ResultsFormatter rf(n * n * n  * 2.0 / 3.0, n * n);
 
-  std::cout << "Building system: " << std::endl;
+  if (verbose)
+    std::cout << "Building system: " << std::endl;
   System s(n);
+  s.init();
 
-  cout << "Initial: " << endl;
-  s.a.print_info();
+  if (verbose) {
+    cout << "Initial: " << endl;
+    s.a.print_info();
+  }
 
-  std::cout << "Solving: " << std::endl;
+  if (verbose)
+    std::cout << "Solving: " << std::endl;
   rf.startTiming();
   auto got = lusolve(s.a, s.b);
   rf.setCpuTime(rf.stopTiming());
 
-  cout << endl << "Final: " << endl;
+  if (verbose)
+    cout << endl << "Final: " << endl;
   //s.a.print();
   if (s.checkSolution(got)) {
     std::cout << "Test passed " << std::endl;
-    return 0;
+  } else {
+    std::cout << "Test failed " << std::endl;
   }
-  rf.print();
-  std::cout << "Test failed " << std::endl;
-  //std::cout << "Rhs: " << std::endl;
-  //print_vector(s.b);
-  //std::cout << "Exp: " << std::endl;
-  //print_vector(s.x);
-  //std::cout << "Got: " << std::endl;
-  //print_vector(got);
-  return 1;
+  if (verbose)
+    rf.print();
+}
+
+// test Gaussian elimination with partial pivoting
+void test_pp_ge(bool verbose = false) {
+  long n = 10;
+
+  ResultsFormatter rf(n * n * n  * 2.0 / 3.0, n * n);
+
+  if (verbose)
+    std::cout << "Building system: " << std::endl;
+  GEPPSystem s(n);
+  s.init();
+
+  if (verbose) {
+    cout << "Initial: " << endl;
+    s.a.print_info();
+    s.a.print();
+  }
+
+  if (verbose)
+    std::cout << "Solving: " << std::endl;
+  rf.startTiming();
+  auto got = lusolve(s.a, s.b);
+  rf.setCpuTime(rf.stopTiming());
+
+  if (verbose)
+    cout << endl << "Final: " << endl;
+  //s.a.print();
+  if (s.checkSolution(got)) {
+    std::cout << "Test passed " << std::endl;
+  } else {
+    std::cout << "Test failed " << std::endl;
+  }
+  if (verbose)
+    rf.print();
+}
+
+int main(void) {
+  test_simple_ge();
+  test_pp_ge();
+  return 0;
 }
