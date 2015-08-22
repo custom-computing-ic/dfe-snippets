bfbe1767759bf802d9e75167d69c86d846dd68d1
Paul Grigoras <paul.grigoras90@gmail.com>
2015-08-22 09:57:51 +0100

    FetchSubTuple based solution.

--- test/LinearAlgebra/ParallelCsrDecoder/src/ParallelCsrDecoderCpuCode.cpp
+++ test/LinearAlgebra/ParallelCsrDecoder/src/ParallelCsrDecoderCpuCode.cpp
@@ -9,23 +9,35 @@
 int main(void)
 {
 
-  const int inSize = 384;
+  const int lengthSize = 32 * 32;
+  const int inputWidth = ParallelCsrDecoder_inputWidth;
+  const int inSize = lengthSize * inputWidth;
 
-  std::vector<int> a(inSize), out(inSize, 0);
+  std::vector<double> a(inSize), out(inSize, 0);
 
   for(int i = 0; i < inSize; ++i) {
     a[i] = i + 1;
   }
 
   std::cout << "Running on DFE." << std::endl;
-  ParallelCsrDecoder(inSize, &a[0], &out[0]);
+  std::vector<int> length;
 
-  for (int i = 0; i < inSize; i++)
-    if (out[i] != a[i]) {
-      printf("Output from DFE did not match CPU: %d : %d != %d\n",
-        i, out[i], a[i]);
-      return 1;
-    }
+  for (int i = 0; i < lengthSize; i++)
+    length.push_back(i % 3 + 1);
+
+  int ticks = lengthSize;
+  ParallelCsrDecoder(
+      ticks,
+      &a[0],
+      &length[0],
+      &out[0]);
+
+  for (int i = 0; i < ticks * inputWidth; ++i) {
+    if (i % inputWidth == 0)
+      std::cout << std::endl;
+    std::cout << out[i] << " ";
+  }
+
 
   std::cout << "Test passed!" << std::endl;
   return 0;
